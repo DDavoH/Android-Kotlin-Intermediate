1ce2208f998d5c54ed882374a4279a4fc25d88a6
DDavoH <davoh9785@gmail.com>
2020-10-26 21:48:48 -0600

    listAdapter base created

--- List_Adapter/app/src/main/java/com/davoh/list_adapter/ListAdapter.kt
+++ List_Adapter/app/src/main/java/com/davoh/list_adapter/ListAdapter.kt
@@ -1,2 +1,25 @@
 package com.davoh.list_adapter
 
+import android.content.Context
+import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter(private val context: Context,
+                  private val listItems: List<String>,
+                  private val itemClickListener: AdapterView.OnItemClickListener):
+        RecyclerView.Adapter<BaseViewHolder<*>>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
