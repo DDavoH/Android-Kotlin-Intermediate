a935be8872316f8471deef069a76a63f0da085be
DDavoH <davoh9785@gmail.com>
2020-10-26 21:43:02 -0600

    configured project

--- List_Adapter/app/src/main/java/com/davoh/list_adapter/MainActivity.kt
+++ List_Adapter/app/src/main/java/com/davoh/list_adapter/MainActivity.kt
@@ -2,10 +2,19 @@ package com.davoh.list_adapter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListAdapter
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.addItemDecoration(DividerItemDecoration(this,DividerItemDecoration.VERTICAL))
+//        rv.adapter = ListAdapter(this, data, this)
     }
 }
