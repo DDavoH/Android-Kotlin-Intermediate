54213eca65f92d72d1cefb758615dc27facb9648
DDavoH <davoh9785@gmail.com>
2020-10-27 20:20:30 -0600

    ListAdapter Working

--- List_Adapter/app/src/main/java/com/davoh/list_adapter/MainActivity.kt
+++ List_Adapter/app/src/main/java/com/davoh/list_adapter/MainActivity.kt
@@ -2,7 +2,8 @@ package com.davoh.list_adapter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ListAdapter
+import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,15 +15,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val listItems = mutableListOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
-        for (x in 6..30){
+        for (x in 6..10){
             listItems.add("Item $x")
         }
 
 
-
         rv.layoutManager = LinearLayoutManager(this)
         rv.addItemDecoration(DividerItemDecoration(this,DividerItemDecoration.VERTICAL))
-        rv.adapter = ListAdapter(this, listItems)
+        val adapter = RvListAdapter()
+        rv.adapter = adapter
+        adapter.submitList(listItems)
+
+        fab.setOnClickListener {
+            val newList = ArrayList<String>()
+            newList.addAll(listItems)
+            val newItemValue = listItems.size + 1
+            newList.add("Item $newItemValue")
+
+            listItems.clear()
+            listItems.addAll(newList)
+
+            adapter.submitList(newList)
+
+            Log.d("Debugeando", "onClick")
+        }
     }
 
 
---
+++ List_Adapter/app/src/main/java/com/davoh/list_adapter/RvListAdapter.kt
@@ -0,0 +1,44 @@
+package com.davoh.list_adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_row.view.*
+
+class RvListAdapter: ListAdapter<String, RvListAdapter.ItemViewHolder>(DiffCallback()){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+       return ItemViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_row, parent, false)
+       )}
+
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: String) = with(itemView) {
+            // TODO: Bind the data with View
+            itemView.textView.text = item
+            setOnClickListener {
+                // TODO: Handle on click
+            }
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+            return false
+        }
+
+        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+            return false
+        }
+
+    }
+
+}
