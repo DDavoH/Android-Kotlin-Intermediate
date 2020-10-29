056870ed4dda1c65fa751f35a33d28dd583d50a4
DDavoH <davoh9785@gmail.com>
2020-10-28 20:45:47 -0600

    shared_preferences works

--- Shared_preferences/app/src/main/java/com/davoh/shared_preferences/MainActivity.kt
+++ Shared_preferences/app/src/main/java/com/davoh/shared_preferences/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.davoh.shared_preferences
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPref = this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+        edit_text.setText(sharedPref.getString("stringKey", "default"))
+
+
+        save_button.setOnClickListener {
+            with (sharedPref.edit()) {
+                putString("stringKey", edit_text.text.toString())
+                apply()
+            }
+        }
+
     }
 }
