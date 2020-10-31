35a316cda07d9e732cdb87eb5ceae321f1ec5b64
DDavoH <davoh9785@gmail.com>
2020-10-31 17:56:35 -0600

    location_permission permission request

--- Location_Permission/app/src/main/java/com/davoh/locationpermission/MainActivity.kt
+++ Location_Permission/app/src/main/java/com/davoh/locationpermission/MainActivity.kt
@@ -1,11 +1,87 @@
 package com.davoh.locationpermission
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestPermission()
+    }
+
+    
+    private fun requestPermission() {
+
+        when {
+            ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+                //performAction(...)
+                Toast.makeText(this, "This persission is granted before", Toast.LENGTH_SHORT).show()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected. In this UI,
+            // include a "cancel" or "no thanks" button that allows the user to
+            // continue using your app without granting the permission.
+            //showInContextUI(...)
+                Toast.makeText(this, "This persission is not granted", Toast.LENGTH_SHORT).show()
+                requestPermissions( arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PEMISSION_CODE_ACCES_FINE_LOCATION)
+        }
+            else -> {
+                // You can directly ask for the permission.
+                requestPermissions( arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PEMISSION_CODE_ACCES_FINE_LOCATION)
+            }
+        }
+
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            PEMISSION_CODE_ACCES_FINE_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                                grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    Toast.makeText(this, "This persission is granted", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "This is a feature unavaible because require a permission", Toast.LENGTH_SHORT).show()
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+
+    }
+
+    companion object {
+        private const val PEMISSION_CODE_ACCES_FINE_LOCATION = 110
     }
 }
